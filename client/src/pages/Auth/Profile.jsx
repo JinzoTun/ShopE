e93c15a1dc5e1d79a0b02ea7032280a7ba3b37d2
Koussay Hajri <hajrikoussay03@gmail.com>
2024-06-08 11:29:47 +0100

    chore: Add logout functionality and handle token blacklisting

--- client/src/pages/Auth/Profile.jsx
+++ client/src/pages/Auth/Profile.jsx
@@ -1,19 +1,38 @@
-import { useState, useEffect } from 'react';
-import { useAuth } from '../../context/useAuth';
-import { useNavigate } from 'react-router-dom';
-import cookies from 'js-cookie';
+import { useState, useEffect } from "react";
+import { useAuth } from "../../context/useAuth";
+import { useNavigate } from "react-router-dom";
+import cookies from "js-cookie";
+import axios from "axios";
 
 const Profile = () => {
   const { user, isAdmin } = useAuth();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // send token to server to blacklist
+    axios
+      .post(`${import.meta.env.VITE_SERVER}/api/users/logout`, null, {
+        headers: {
+          Authorization: `Bearer ${cookies.get("jwt")}`,
+        },
+      })
+      .then(() => {
+        cookies.remove("jwt");
+        navigate("/");
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     if (user) {
       setLoading(false);
     } else {
-      setError('User not found.');
+      setError("User not found.");
       setLoading(false);
     }
   }, [user]);
@@ -28,15 +47,15 @@ const Profile = () => {
           <p className="text-center text-red-500">{error}</p>
         ) : user ? (
           <div>
-            <p className="mb-2"><strong>Name:</strong> {user.name}</p>
-            <p className="mb-2"><strong>Email:</strong> {user.email}</p>
+            <p className="mb-2">
+              <strong>Name:</strong> {user.name}
+            </p>
+            <p className="mb-2">
+              <strong>Email:</strong> {user.email}
+            </p>
             <button
               className="bg-red-500 text-white px-4 py-2 rounded mt-4 w-full"
-              onClick={() => {
-                cookies.remove('jwt');
-                navigate('/');
-                window.location.reload();
-              }}
+              onClick={handleLogout}
             >
               Logout
             </button>
@@ -46,7 +65,7 @@ const Profile = () => {
                 <button
                   className="bg-red-500 text-white px-4 py-2 rounded mt-4 w-full"
                   onClick={() => {
-                    navigate('/admin');
+                    navigate("/admin");
                   }}
                 >
                   Admin Dashboard
